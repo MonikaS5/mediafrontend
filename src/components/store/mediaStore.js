03d7ce660c0d6bd2aefacf6009f8e876c149882a
MonikaS5 <monika.bhagade@gmail.com>
2024-11-01 23:03:14 +0000

    axios and fetchMediaItems

--- src/components/store/mediaStore.js
+++ src/components/store/mediaStore.js
@@ -1,5 +1,19 @@
 import { create } from 'zustand';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/media'
 
 const useMediaStore = create((set) => ({
     mediaItems: [],
+
+    fetchMediaItems: async () => {
+        try {
+            const response = await axios.get(API_URL);
+            set({ mediaItems: response.data })
+        } catch (error) {
+
+        }
+    }
 }));
+
+export default useMediaStore;
